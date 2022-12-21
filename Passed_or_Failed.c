ac6a467968337e1d81e14fa7f2f8c7b6c674819e
Anirban Khan <119731572+Anirban2004@users.noreply.github.com>
2022-12-22 00:22:19 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     if(a>=35&&b>=35&&c>=35&&d>=35&&e>=35)     {         printf("PASSED");     }     else if(a<35||b<35||c<35||d<35||e<35)     {         printf("FAILED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    if(a>=35&&b>=35&&c>=35&&d>=35&&e>=35)
+    {
+        printf("PASSED");
+    }
+    else if(a<35||b<35||c<35||d<35||e<35)
+    {
+        printf("FAILED");
+    }
+}
+
